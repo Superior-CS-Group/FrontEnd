7bf6d93a76bb080a7584ea4939c805c8f87a4c3f
Mountain-Sky <owens@superiorbizsolutions.com>
2021-11-11 06:22:25 +0200

    Updated sidebar

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -31,24 +31,8 @@ const renderBody = (item, index) => (
 const Customers = () => {
     return (
         <div>
-            <h2 className="page-header">
-                CRM
-            </h2>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card__body">
-                            <Table
-                                limit='10'
-                                headData={customerTableHead}
-                                renderHead={(item, index) => renderHead(item, index)}
-                                bodyData={customerList}
-                                renderBody={(item, index) => renderBody(item, index)}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+           
+           
         </div>
     )
 }
