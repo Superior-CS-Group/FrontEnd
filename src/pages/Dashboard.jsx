00faa9306fda4c63d2d221ad3fd141b604a835b2
Mountain-Sky <owens@superiorbizsolutions.com>
2021-11-11 05:45:22 +0200

    Initial Commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react'
+
+
+
+import { useSelector } from 'react-redux'
+
+
+
+const Dashboard = () => {
+
+    const themeReducer = useSelector(state => state.ThemeReducer.mode)
+
+    return (
+        <div>
+           
+           
+        </div>
+    )
+}
+
+export default Dashboard
