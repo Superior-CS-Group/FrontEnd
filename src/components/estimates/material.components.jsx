97115ed644fdbd9fe5567af3bf1691feca5b48ac
tony-onepercent <tonyc@superiorcsgroup.com>
2021-11-17 19:09:34 +0530

    updated

--- src/components/estimates/material.components.jsx
+++ src/components/estimates/material.components.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button, Select, Input, Form, Row, Col, Upload, message } from "antd";
 import { LinkOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { postData } from "../../utils/fetchApi";
 
 const { Dragger } = Upload;
 
@@ -27,7 +28,119 @@ const props = {
     console.log("Dropped files", e.dataTransfer.files);
   },
 };
+
 export default class Material extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      hours: "",
+      days: "",
+      rate: "",
+      vname: "",
+      price: "",
+      unit: "",
+      type: "material",
+      message: "",
+      isRedirect: false,
+      isValidEmail: false,
+      isLoading: false,
+      errors: {},
+      visible: false,
+      isMaterial: true,
+      isProduct: true,
+      variation: [],
+    };
+  }
+  validateEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  validateFields = () => {
+    const errors = {};
+    if (!this.state.name) {
+      errors.name = "Material Name is not blank";
+    }
+    if (!this.state.vname) {
+      errors.vname = "Variation Name is not blank";
+    }
+    if (!this.state.price) {
+      errors.price = "Variation Price is not blank";
+    }
+    if (!this.state.unit) {
+      errors.unit = "Variation Unit is not blank";
+    }
+    return {
+      errors,
+      isValid: !Object.keys(errors).length,
+    };
+  };
+
+  handleAllChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleVariationAdd = (e) => { 
+    e.preventDefault();
+    this.setState({ errors: {} });
+    const { errors, isValid } = this.validateFields();
+    if (!isValid) {
+      this.setState({ errors });
+      return;
+    }
+
+    const newVariation = {
+      name: this.state.vname,
+      price: this.state.price,
+      unit: this.state.unit,
+    };
+    this.setState({
+      variation: [newVariation, ...this.state.variation],
+      name: "",
+      price: "",
+      unit: "",
+    });
+    console.log(newVariation); 
+    console.log(this.state.variation); 
+  };
+  
+  handleSubmit = async (event) => {
+    // console.log(localStorage.getItem("token"))
+
+    event.preventDefault();
+    this.setState({ errors: {} });
+    const { errors, isValid } = this.validateFields();
+    if (!isValid) {
+      this.setState({ errors });
+      return;
+    }
+    
+    const { name, hours, days, rate, type, variation } = this.state;
+    const body = { name, hours, days, rate, type, variation };
+    console.log("body: ", body);
+
+    try {
+      // const result = await postData(`services/add`, body);
+      // // console.log("result: ", result);
+      // this.setState({
+      //   message: "New Material Added!",
+      //   isRedirect: true,
+      //   isLoading: false,
+      // });
+    } catch (err) {
+      console.log("error", err, err.response);
+
+      this.setState({
+        errors: err.response.data.errors,
+        isLoading: false,
+      });
+    }
+  };
+
   render() {
     return (
       <>
@@ -39,7 +152,12 @@ export default class Material extends Component {
                   placeholder={this.props.placeholder}
                   value={this.props.value1}
                   className="ant-modal-input"
+                  name="name"
+                  onChange={this.handleAllChange}
                 />
+                <div role="alert" class="text-danger">
+                  {this.state.errors.name}
+                </div>
               </Form.Item>
             </Col>
             <div
@@ -53,7 +171,12 @@ export default class Material extends Component {
                       placeholder={this.props.standard}
                       value={this.props.value2}
                       className="ant-modal-input"
+                      name="vname"
+                      onChange={this.handleAllChange}
                     />
+                    <div role="alert" class="text-danger">
+                      {this.state.errors.vname}
+                    </div>
                   </Form.Item>
                 </Col>
                 <Col span={24} lg={8}>
@@ -62,7 +185,12 @@ export default class Material extends Component {
                       placeholder={this.props.unitplaceholder}
                       value={this.props.value3}
                       className="ant-modal-input"
+                      name="unit"
+                      onChange={this.handleAllChange}
                     />
+                    <div role="alert" class="text-danger">
+                      {this.state.errors.unit}
+                    </div>
                   </Form.Item>
                 </Col>
                 <Col span={24} lg={8}>
@@ -71,7 +199,12 @@ export default class Material extends Component {
                       placeholder={this.props.costplaceholder}
                       value={this.props.value4}
                       className="ant-modal-input"
-                    />
+                      name="price"
+                      onChange={this.handleAllChange}
+                    />{" "}
+                    <div role="alert" class="text-danger">
+                      {this.state.errors.price}
+                    </div>
                   </Form.Item>
                 </Col>
 
@@ -94,13 +227,13 @@ export default class Material extends Component {
               </Row>
 
               <span className="text-primary rounded-circle ms-2 mt-50">
-                <PlusCircleOutlined style={{ fontSize: "24px" }} />
+                <PlusCircleOutlined style={{ fontSize: "24px" }} onClick={this.handleVariationAdd}/>
               </span>
             </div>
             <Col span={24}>
               <Form.Item className="text-center mt-4 mb-0">
                 <Button className="ant-cancel-btn me-3">Cancel</Button>
-                <Button type="primary" className="ant-add-button">
+                <Button type="primary" className="ant-add-button" onClick={this.handleSubmit}>
                   Add to Catalog
                 </Button>
               </Form.Item>
--- src/components/estimates/services.components.jsx
+++ src/components/estimates/services.components.jsx
@@ -1,11 +1,96 @@
 import React, { Component } from "react";
 import { Button, Input, Form, Row, Col } from "antd";
 import { LinkOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { postData } from "../../utils/fetchApi";
 
 function handleChange(value) {
   console.log(`selected ${value}`);
 }
 export default class Services extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      hours: "",
+      days: "",
+      rate: "",
+      type:"service",
+      message: "",
+      isRedirect: false,
+      isValidEmail: false,
+      isLoading: false,
+      errors: {},
+      visible: false,
+      isMaterial: true,
+      isProduct: true,
+    };
+  }
+  validateEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  validateFields = () => {
+    const errors = {};
+    if (!this.state.name) {
+      errors.name = "Service Name is not blank";
+    }
+    if (!this.state.hours) {
+      errors.hours = "Service Hour is not blank";
+    }
+    if (!this.state.days) {
+      errors.days = "Service Day is not blank";
+    }
+    if (!this.state.rate) {
+      errors.rate = "Service Rate is not blank";
+    }
+    return {
+      errors,
+      isValid: !Object.keys(errors).length,
+    };
+  };
+
+  handleAllChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = async (event) => {
+
+    // console.log(localStorage.getItem("token"))
+    
+    event.preventDefault();
+    this.setState({ errors: {} });
+    const { errors, isValid } = this.validateFields();
+    if (!isValid) {
+      this.setState({ errors });
+      return;
+    }
+    this.setState({ isLoading: true });
+    const { name, hours, days, rate,type } = this.state;
+    const body = { name, hours, days, rate,type };
+    // console.log("body: ", body);
+
+    try {
+      const result = await postData(`services/add`, body);
+      // console.log("result: ", result);
+      this.setState({ 
+        message: "New Service Added!",
+        isRedirect: true,
+        isLoading: false,
+      });
+    } catch (err) {
+      console.log("error", err, err.response);
+
+      this.setState({
+        errors: err.response.data.errors,
+        isLoading: false,
+      });
+    }
+  };
+
   render() {
     return (
       <>
@@ -14,32 +99,72 @@ export default class Services extends Component {
             <Col span={24} lg={24}>
               <Form.Item label="Services Name" className="ant-select-large">
                 <Input
-                  placeholder={this.props.servicesNamePlaceholder} value={this.props.valueService}
+                  placeholder={this.props.servicesNamePlaceholder}
+                  value={this.props.valueService}
                   className="ant-modal-input"
+                  name="name"
+                  onChange={this.handleAllChange}
                 />
+                <div role="alert" class="text-danger">
+                  {this.state.errors.name}
+                </div>
               </Form.Item>
             </Col>
 
             <Col span={24} lg={8}>
               <Form.Item label="Hours">
-                <Input placeholder={this.props.hours} value={this.props.hoursValue} className="ant-modal-input" />
+                <Input
+                  placeholder={this.props.hours}
+                  value={this.props.hoursValue}
+                  className="ant-modal-input"
+                  name="hours"
+                  onChange={this.handleAllChange}
+                />
+                <div role="alert" class="text-danger">
+                  {this.state.errors.hours}
+                </div>
               </Form.Item>
             </Col>
             <Col span={24} lg={8}>
               <Form.Item label="Days">
-                <Input placeholder={this.props.days} value={this.props.daysValue} className="ant-modal-input" />
+                <Input
+                  placeholder={this.props.days}
+                  value={this.props.daysValue}
+                  className="ant-modal-input"
+                  name="days"
+                  onChange={this.handleAllChange}
+                />
+                <div role="alert" class="text-danger">
+                  {this.state.errors.days}
+                </div>
               </Form.Item>
             </Col>
             <Col span={24} lg={8}>
               <Form.Item label="Production Rate">
-                <Input placeholder={this.props.productionRate} value={this.props.rate} className="ant-modal-input" />
+                <Input
+                  placeholder={this.props.productionRate}
+                  value={this.props.rate}
+                  className="ant-modal-input"
+                  name="rate"
+                  onChange={this.handleAllChange}
+                />
+                <div role="alert" class="text-danger">
+                  {this.state.errors.rate}
+                </div>
               </Form.Item>
             </Col>
 
             <Col span={24}>
               <Form.Item className="text-center mt-4 mb-0">
+              <div role="alert" class="text-success">
+                  {this.state.message}
+                </div>
                 <Button className="ant-cancel-btn me-3">Cancel</Button>
-                <Button type="primary" className="ant-add-button">
+                <Button
+                  type="primary"
+                  className="ant-add-button"
+                  onClick={this.handleSubmit}
+                >
                   Add to Catalog
                 </Button>
               </Form.Item>
