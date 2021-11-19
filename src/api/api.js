1f98b511654f1efe2f991e186d84872600549a1e
Saral Shrivastava <shrivastavaa278@gmail.com>
2021-11-19 14:39:57 +0530

    removed console logs

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,6 @@ const axiosInstance = async () => {
 
 const parseResponse = (response) => {
   const data = JSON.parse(response);
-  console.log("data: ", data);
   if (data?.errors) {
     return {
       remote: "failure",
@@ -39,13 +38,10 @@ const request = async (config) => {
     const response = await instance.request({
       ...config,
       transformResponse: (res) => {
-        console.log("beforeResp: ", res);
         const resp = parseResponse(res);
-        console.log("resppp: ", resp);
         return resp.remote === "success" ? resp.data : resp;
       },
     });
-    console.log("resqu: ", response, response.data);
     return {
       remote: "success",
       data: response.data,
