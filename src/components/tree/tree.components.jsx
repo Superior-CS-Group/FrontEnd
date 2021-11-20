89f37fd689b2ce4ebdd6ddbc21c95d95e2443a80
sunilsingh9131 <sunil.baghel@digimonk.in>
2021-11-20 13:33:30 +0530

    sunil tree modal

--- src/components/tree/tree.components.jsx
+++ src/components/tree/tree.components.jsx
@@ -3,25 +3,23 @@ import "./tree.css";
 import { Card, Row, Col, Form, Input, Button, Select, Drawer } from "antd";
 import BreadcrumbBar from "../breadcrumb/Breadcrumb.pages";
 import { treeIcon } from "../../utils/svg.file";
-import { EditOutlined, PlusOutlined } from "@ant-design/icons";
+import { EditOutlined, CloseOutlined } from "@ant-design/icons";
 
 export default function Tree() {
   const { Option } = Select;
 
-  const [visible, setVisible] = useState(false);
-  const showDrawer = () => {
-    console.log("visible");
-    setVisible(true);
+  const [tree, setTree] = useState(false);
+  const addTree = () => {
+    setTree(true);
   };
   const onClose = () => {
-    console.log("close floter");
-    setVisible(false);
+    setTree(false);
   };
 
   return (
     <>
       <BreadcrumbBar name="Demolition and Prep" breaclass="mb-4" />
-      <Card>
+      <Card className="r-height">
         <div className="tree mb-5">
           <ul className="d-flex">
             <li className="mx-auto">
@@ -29,7 +27,7 @@ export default function Tree() {
                 Demolition & Prep. <EditOutlined />
               </a>
               <div className="d-flex align-items-center justify-content-center">
-                <span className="ant-cricle-add" onClick={showDrawer}>
+                <span className="ant-cricle-add" onClick={addTree}>
                   {treeIcon}
                 </span>
               </div>
@@ -70,71 +68,82 @@ export default function Tree() {
             </li>
           </ul>
         </div>
-
-        <Drawer
-          title={[
-            <>
-              <h5 className="text-center mb-0">Define Formula</h5>
-            </>,
-          ]}
-          placement="bottom"
-          onClose={onClose}
-          visible={visible}
-          className="bottom-modal"
-        >
-          <Form layout="vertical">
-            <Row gutter={[24, 0]} className="mb-3">
-              <Col lg={4}>
-                <Select
-                  size="large"
-                  className="ant-formulla"
-                  defaultValue="User Input"
-                  style={{ width: "100%", borderRadius: "9px" }}
-                >
-                  <Option value="User Input">User Input</Option>
-                  <Option value="User Input">User Input</Option>
-                  <Option value="User Input">User Input</Option>
-                  <Option value="User Input">User Input</Option>
-                </Select>
-              </Col>
-              <Col lg={16}>
-                <Input
-                  placeholder="length of per pipe"
-                  className="radius-9"
-                  size="large"
-                />
-              </Col>
-              <Col lg={4}>
-                <Button type="primary" size="large" className="radius-9">
-                  Add Element
-                </Button>
-              </Col>
-            </Row>
-            <Row gutter={[24, 0]}>
-              <Col lg={20}>
-                <Form.Item>
-                  <div className="radius-9 ant-blue-add-tree-box">
-                    <ul>
-                      <li>
-                        <span>Perf pipe cost</span>{" "}
-                        <span className="tag-close-icon">x</span>
-                      </li>
-                      <li>
-                        <span>Multiply {"{*}"}</span>{" "}
-                        <span className="tag-close-icon">x</span>
-                      </li>
-                    </ul>
-                  </div>
-                </Form.Item>
-              </Col>
-              <Col lg={4}>
-                <Button type="primary" size="large" className="radius-9">
-                  Save Formula
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Drawer>
+        {tree ? (
+          <div className="bottom-modal p-4 pt-3">
+            <Form layout="vertical">
+              <Row gutter={[24, 0]} className="mb-3">
+                <Col span={24}>
+                  <h5 className="text-center mb-4">
+                    Define Formula{" "}
+                    <span className="float-end closeicon" onClick={onClose}>
+                      <CloseOutlined />
+                    </span>
+                  </h5>
+                </Col>
+                <Col lg={4}>
+                  <Select
+                    size="large"
+                    className="ant-formulla"
+                    defaultValue="User Input"
+                    style={{ width: "100%", borderRadius: "9px" }}
+                  >
+                    <Option value="User Input">User Input</Option>
+                    <Option value="User Input">User Input</Option>
+                    <Option value="User Input">User Input</Option>
+                    <Option value="User Input">User Input</Option>
+                  </Select>
+                </Col>
+                <Col lg={16}>
+                  <Input
+                    placeholder="length of per pipe"
+                    className="radius-9"
+                    size="large"
+                  />
+                </Col>
+                <Col lg={4}>
+                  <Button
+                    type="primary"
+                    size="large"
+                    className="radius-9"
+                    block
+                  >
+                    Add Element
+                  </Button>
+                </Col>
+              </Row>
+              <Row gutter={[24, 0]}>
+                <Col lg={20}>
+                  <Form.Item>
+                    <div className="radius-9 ant-blue-add-tree-box">
+                      <ul>
+                        <li>
+                          <span>Perf pipe cost</span>{" "}
+                          <span className="tag-close-icon">x</span>
+                        </li>
+                        <li>
+                          <span>Multiply {"{*}"}</span>{" "}
+                          <span className="tag-close-icon">x</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </Form.Item>
+                </Col>
+                <Col lg={4}>
+                  <Button
+                    type="primary"
+                    size="large"
+                    className="radius-9"
+                    block
+                  >
+                    Save Formula
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        ) : (
+          ""
+        )}
       </Card>
     </>
   );
