388b2171d00789b81ecf60acdf429d6fc6c81261
pradeeptomar21 <pradeep.tomar@digimonk.in>
2021-11-29 15:16:18 +0530

    delete popup

--- src/components/estimates/estimates.datatable.components.jsx
+++ src/components/estimates/estimates.datatable.components.jsx
@@ -12,7 +12,7 @@ import fillter from "../../images/fillter.png";
 
 export default function Datatable() {
   const params = useParams();
-
+  const [ShowDeleteModal, setShowDeleteModal] = useState(false);
   const [state, setState] = useState({
     estimateResults: [],
     data: [],
@@ -266,6 +266,7 @@ export default function Datatable() {
                 className="me-2 cursor-btn"
                 onClick={(e) => {
                   DeleteModal(customerData[0]._id, "customerLead");
+                  setShowDeleteModal(true);
                 }}
               >
                 {Datel}
@@ -312,6 +313,18 @@ export default function Datatable() {
     });
     console.log("filterData: ", filteredData);
     setState({ ...state, filteredData, filter: value });
+  };
+
+  const showModal = () => {
+    setShowDeleteModal(true);
+  };
+
+  const handleOk = () => {
+    setShowDeleteModal(false);
+  };
+
+  const handleCancel = () => {
+    setShowDeleteModal(false);
   };
 
   return (
--- src/components/modal/deleteModal.component.jsx
+++ src/components/modal/deleteModal.component.jsx
@@ -1,24 +1,35 @@
-import { Modal, Button, Space } from "antd";
+import { Modal, Button, Space, Row, Col } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { deleteCustomerLead } from "../../api/delete";
 
 const { confirm } = Modal;
 
-export default function DeleteModal(removeId, table, updateData) {
-  confirm({
-    title: "Do you Want to delete these items?",
-    icon: <ExclamationCircleOutlined />,
+export default function DeleteModal(removeId, table, updateData, props) {
+  // confirm({
+  //   title: "Do you Want to delete these items?",
+  //   icon: <ExclamationCircleOutlined />,
+  //   content: "Some descriptions",
+  //   onOk() {
+  //     if (table === "customerLead") {
+  //       const body = { id: removeId };
+  //       console.log("OK", body);
+  //       deleteCustomerLead(body);
+  //     }
+  //   },
+  //   onCancel() {
+  //     console.log("Cancel");
+  //   },
+  // });
 
-    content: "Some descriptions",
-    onOk() {
-      if (table === "customerLead") {
-        const body = { id: removeId };
-        console.log("OK", body);
-        deleteCustomerLead(body);
-      }
-    },
-    onCancel() {
-      console.log("Cancel");
-    },
-  });
+  <Modal title="Warning!" visible={props.ShowDeleteModal} onOk={props.handleOk}>
+    <p>{props.content}</p>
+    <Row>
+      <Col md={12}>
+        <Button>Cancel</Button>
+      </Col>
+      <Col md={12}>
+        <Button>Delete</Button>
+      </Col>
+    </Row>
+  </Modal>;
 }
