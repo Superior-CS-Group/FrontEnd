db359acec69b62e46cacfb2e8c3359926ff337c6
sunilsingh9131 <sunil.baghel@digimonk.in>
2021-12-14 19:26:16 +0530

    update

--- src/components/auth/Setup.jsx
+++ src/components/auth/Setup.jsx
@@ -1,10 +1,65 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Steps, Button, Card, message } from "antd";
 export default function Setup() {
+  const { Step } = Steps;
+
+  const steps = [
+    {
+      title: "First",
+      content: "First-content",
+    },
+    {
+      title: "Second",
+      content: "Second-content",
+    },
+    {
+      title: "Last",
+      content: "Last-content",
+    },
+  ];
+  const [current, setCurrent] = React.useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
   return (
     <>
       <div className="login-bg ">
-        <Container>Setup page</Container>
+        <Container>
+          <Card>
+            <Steps current={current}>
+              {steps.map((item) => (
+                <Step key={item.title} title={item.title} />
+              ))}
+            </Steps>
+            <div className="steps-content">{steps[current].content}</div>
+            <div className="steps-action">
+              {current < steps.length - 1 && (
+                <Button type="primary" onClick={() => next()}>
+                  Next
+                </Button>
+              )}
+              {current === steps.length - 1 && (
+                <Button
+                  type="primary"
+                  onClick={() => message.success("Processing complete!")}
+                >
+                  Done
+                </Button>
+              )}
+              {current > 0 && (
+                <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+                  Previous
+                </Button>
+              )}
+            </div>
+          </Card>
+        </Container>
       </div>
     </>
   );
