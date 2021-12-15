9d87ade19bbf0a42e7c9848c02cbd112d4d190e9
sunilsingh9131 <sunil.baghel@digimonk.in>
2021-12-15 15:51:12 +0530

    setup page

--- src/components/auth/Setup.jsx
+++ src/components/auth/Setup.jsx
@@ -1,66 +1,243 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { Steps, Button, Card, message } from "antd";
-export default function Setup() {
-  const { Step } = Steps;
-
-  const steps = [
-    {
-      title: "First",
-      content: "First-content",
-    },
-    {
-      title: "Second",
-      content: "Second-content",
-    },
-    {
-      title: "Last",
-      content: "Last-content",
-    },
-  ];
-  const [current, setCurrent] = React.useState(0);
-
-  const next = () => {
-    setCurrent(current + 1);
+import React, { Component } from "react";
+import {
+  Steps,
+  Card,
+  Form,
+  Input,
+  Row,
+  Col,
+  Button,
+  Switch,
+  Select,
+} from "antd";
+import profile from "../../images/profile.png";
+const { Step } = Steps;
+const { Option } = Select;
+export default class Setup extends Component {
+  state = {
+    current: 0,
+  };
+  onChange = (current) => {
+    console.log("onChange:", current);
+    this.setState({ current });
   };
 
-  const prev = () => {
-    setCurrent(current - 1);
+  next = () => {
+    this.setState({
+      current: this.state.current + 1,
+    });
   };
-  return (
-    <>
-      <div className="login-bg ">
-        <Container>
-          <Card>
-            <Steps current={current}>
-              {steps.map((item) => (
-                <Step key={item.title} title={item.title} />
-              ))}
-            </Steps>
-            <div className="steps-content">{steps[current].content}</div>
-            <div className="steps-action">
-              {current < steps.length - 1 && (
-                <Button type="primary" onClick={() => next()}>
-                  Next
-                </Button>
+
+  prev = () => {
+    this.setState({
+      current: this.state.current - 1,
+    });
+  };
+
+  render() {
+    function handleChange(value) {
+      console.log(`selected ${value}`);
+    }
+
+    function onChange(checked) {
+      console.log(`switch to ${checked}`);
+    }
+    const { current } = this.state;
+    return (
+      <div className="login-bg py-3">
+        <div className="mx-auto" style={{ maxWidth: "800px" }}>
+          <Card className="radius-9" bordered={false}>
+            <div className="mb-3">
+              <Steps
+                labelPlacement="vertical "
+                current={current}
+                onChange={this.onChange}
+                className="step-cricle"
+              >
+                <Step title="Company info" key={0} />
+                <Step title="Personal info" key={1} />
+                <Step title="Add all users" key={2} />
+                {/* <Step title="Profile" key={3} /> */}
+              </Steps>
+            </div>
+            <div>
+              {current === 0 && (
+                <>
+                  <Form layout="vertical">
+                    <Form.Item label="Company Name" className="mb-3">
+                      <Input
+                        placeholder="Enter your Company Name"
+                        className="radius-9"
+                        size="large"
+                      />
+                    </Form.Item>
+
+                    <Form.Item label="Company Address" className="mb-3">
+                      <Input.TextArea
+                        placeholder="Enter yourCompany Address "
+                        className="radius-9 h-150"
+                      />
+                    </Form.Item>
+                    <Form.Item className="mb-3" label="Logo">
+                      <Input type="file" className="radius-9 " size="large" />
+                    </Form.Item>
+
+                    <Row gutter={[24, 0]}>
+                      <Col span={12}>
+                        <Button
+                          onClick={this.next}
+                          type="primary"
+                          className="px-25"
+                        >
+                          Next
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </>
               )}
-              {current === steps.length - 1 && (
-                <Button
-                  type="primary"
-                  onClick={() => message.success("Processing complete!")}
-                >
-                  Done
-                </Button>
+              {current === 1 && (
+                <>
+                  <Form layout="vertical">
+                    <Form.Item label="Name" className="mb-3">
+                      <Input
+                        placeholder="Enter your name"
+                        className="radius-9"
+                        size="large"
+                      />
+                    </Form.Item>
+                    <Form.Item label="Role" className="mb-3">
+                      <Input
+                        placeholder="Enter your role"
+                        className="radius-9"
+                        size="large"
+                      />
+                    </Form.Item>
+
+                    <Row gutter={[24, 0]}>
+                      <Col lg={6}>
+                        <Form.Item label="Edit notifications" className="mb-3">
+                          <Switch defaultChecked onChange={onChange} />
+                        </Form.Item>
+                      </Col>
+                      <Col lg={6}>
+                        <Form.Item label="Edit your permissions">
+                          <Switch onChange={onChange} />
+                        </Form.Item>
+                      </Col>
+                    </Row>
+
+                    <Form.Item label="Profile" className="mb-3">
+                      <Input type="file" className="radius-9" size="large" />
+                    </Form.Item>
+                    <Form.Item>
+                      <div className="profile-set">
+                        <img src={profile} alt="user" />
+                      </div>
+                    </Form.Item>
+
+                    <div className="pt-3">
+                      <Row gutter={[24, 0]}>
+                        <Col span={12}>
+                          <Button onClick={this.prev} className="px-25">
+                            Prev
+                          </Button>
+                        </Col>
+
+                        <Col span={12} className="text-right">
+                          <Button
+                            onClick={this.next}
+                            type="primary"
+                            className="px-25"
+                          >
+                            Next
+                          </Button>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Form>
+                </>
               )}
-              {current > 0 && (
-                <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
-                  Previous
-                </Button>
+              {current === 2 && (
+                <>
+                  <Form layout="vertical">
+                    <Form.Item label="Name" className="mb-3">
+                      <Input
+                        placeholder="name"
+                        size="large"
+                        className="radius-9"
+                      />
+                    </Form.Item>
+                    <Form.Item label="Email " className="mb-3">
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        className="radius-9"
+                        size="large"
+                      />
+                    </Form.Item>
+                    <Form.Item label="Phone " className="mb-3">
+                      <Input
+                        placeholder="Phone No."
+                        className="radius-9"
+                        size="large"
+                        type="number"
+                      />
+                    </Form.Item>
+                    <Form.Item label="Password" className="mb-3">
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        className="radius-9"
+                        size="large"
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      label="Profile pic (If wanted) "
+                      className="mb-3"
+                    >
+                      <Input type="file" className="radius-9" size="large" />
+                    </Form.Item>
+                    <Form.Item label="Select">
+                      <Select
+                        className="role-user"
+                        size="large"
+                        defaultValue="lucy"
+                        style={{ width: "100%" }}
+                        onChange={handleChange}
+                      >
+                        <Option value="jack">Field employee</Option>
+                        <Option value="lucy">Foreman</Option>
+                        <Option value="disabled">Project manager</Option>
+                        <Option value="Yiminghe">Salesperson</Option>
+                        <Option value="admin">Full Admin</Option>
+                      </Select>
+                    </Form.Item>
+
+                    <Row gutter={[24, 0]}>
+                      <Col span={12}>
+                        <Button onClick={this.prev} className="px-25">
+                          Prev
+                        </Button>
+                      </Col>
+
+                      <Col span={12} className="text-right">
+                        <Button type="primary" className="px-25">
+                          Save
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </>
               )}
+
+              {/* 
+
+              {current === 3 && <>ff</>} */}
             </div>
           </Card>
-        </Container>
+        </div>
       </div>
-    </>
-  );
+    );
+  }
 }
